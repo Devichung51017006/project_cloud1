07a8e9c74f841adc19abb9433e37615578addf0d
Devichung51017006 <48231542+Devichung51017006@users.noreply.github.com>
2020-03-24 15:20:48 +0800

    ada perubahan pada app
    perubahan Hello World menjadi nama,jurusan, dan NIM

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Devi chungiarto, Sistem Informasi- 51017006\n')
 })
 
 app.listen(PORT, HOST)
