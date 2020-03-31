3d6e765066c15c027f6726b3b52f09adf50fcdef
Devichung51017006 <48231542+Devichung51017006@users.noreply.github.com>
2020-03-31 12:46:09 +0800

    Perubahan dalam Rangka KUIS 1
    Kuis pertama mata kuliah Cloud Computing. dengan menambahkan nama, nim dan Jrurusan

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Devi chungiarto, Sistem Informasi- 51017006\n')
+    res.send('Halo Ini ada Website Kedua dengan Kubernetes dalam rangka Quiz 1 Mata Kuliah Cloud Computing\n')
+    res.send('Nama Saya : Devi Chungiarto\n')
+    res.send('NIM Saya  : 51017006\n')
+    res.send('Jurusan Saya : Sistem informasi\n')
 })
 
 app.listen(PORT, HOST)
