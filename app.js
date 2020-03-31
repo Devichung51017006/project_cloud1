5634222d4172574b7b4d14ec78723675ed2b878d
Devichung51017006 <48231542+Devichung51017006@users.noreply.github.com>
2020-03-31 12:55:29 +0800

    Perubahan Kuis
    Kuis 1

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Halo Ini ada Website Kedua dengan Kubernetes dalam rangka Quiz 1 Mata Kuliah Cloud Computing\n')
-    res.send('Nama Saya : Devi Chungiarto\n')
-    res.send('NIM Saya  : 51017006\n')
-    res.send('Jurusan Saya : Sistem informasi\n')
+    res.send('Halo Ini ada Website Kedua dengan Kubernetes dalam rangka Quiz 1 Mata Kuliah Cloud Computing.\n Nama Saya : Devi Chungiarto,\n NIM Saya  : 51017006,\n Jurusan Saya : Sistem informasi \n')
+    
 })
 
 app.listen(PORT, HOST)
